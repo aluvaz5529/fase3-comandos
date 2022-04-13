3a8d27cc4b40e49c114c1f47ce66f5231f8abbba
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-13 17:30:56 +0200

    Update carpetas_publicas.ps1

--- carpetas_publicas.ps1
+++ carpetas_publicas.ps1
@@ -8,11 +8,10 @@ mkdir Enfermeria
 #
 $ruta = 'X:\Departamentos'
 #Creamos la carpeta que hemos añadido en la variable $ruta
-
-New-SmbShare -Name comun -Path $ruta -FullAccess Administradores -ReadAccess SMR-GL-Read-DirPublic `
--ChangeAccess SMR-GL-Change-DirPublico -ConcurrentUserLimit 28 `
--Description "Carpeta publico para el acceso de usuarios"
-#Lo que se me ha ocurrido para esto es crear un grupo que se llame "común" meter los grupos globales a este grupo local
-#entonces
-#está común con permisos de acceso en todos los departamentos con todos los grupos globales dentro
+#
+New-SmbShare -Name comun_x -Path $ruta -FullAccess Administradores -ReadAccess comun_x -ConcurrentUserLimit 28 `
+-Description "Carpeta común para el acceso de usuarios"
+#Lo que se me ha ocurrido para esto es crear un grupo que se llame "comun_x" meter los grupos globales a este grupo local
+#entonces queda
+#común_x con permisos de acceso en todos los departamentos con todos los grupos globales dentro
 #después estan los grupos locales de cada departamento con permisos de lectura y escritura con los grupos locales respectivos
