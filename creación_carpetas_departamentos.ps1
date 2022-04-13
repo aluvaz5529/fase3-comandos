5c82c174b1d4fc4316d2fcab07a2fff3b13c9813
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-13 17:15:02 +0200

    Update creación_carpetas_departamentos.ps1

--- creación_carpetas_departamentos.ps1
+++ creación_carpetas_departamentos.ps1
@@ -9,6 +9,10 @@ mkdir Enfermeria
 $ruta = 'X:\Departamentos'
 #Creamos la carpeta que hemos añadido en la variable $ruta
 
-New-SmbShare -Name Publico-RC -Path $ruta -FullAccess SMR-GL-Total-DirPublico -ReadAccess SMR-GL-Read-DirPublic `
+New-SmbShare -Name Publico-RC -Path $ruta -FullAccess Administradores -ReadAccess SMR-GL-Read-DirPublic `
 -ChangeAccess SMR-GL-Change-DirPublico -ConcurrentUserLimit 28 `
 -Description "Carpeta publico para el acceso de usuarios"
+#Lo que se me ha ocurrido para esto es crear un grupo que se llame "común" meter los grupos globales a este grupo local
+#entonces
+#está común con permisos de acceso en todos los departamentos con todos los grupos globales dentro
+#después estan los grupos locales de cada departamento con permisos de lectura y escritura con los grupos locales respectivos
