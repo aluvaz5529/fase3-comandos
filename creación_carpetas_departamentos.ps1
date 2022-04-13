a9e73cb797ce3d18c22b38e5818fdb000477059e
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-13 13:42:34 +0200

    Update creación_carpetas_departamentos.ps1

--- creación_carpetas_departamentos.ps1
+++ creación_carpetas_departamentos.ps1
@@ -1,7 +1,14 @@
 x:
-mkdir departamentos
-cd departamentos
+mkdir Departamentos
+cd Departamentos
 mkdir Informatica
 mkdir Comunicacion_Prensa
 mkdir Alergologia
 mkdir Enfermeria
+#
+$ruta = 'X:\Departamentos'
+#Creamos la carpeta que hemos añadido en la variable $ruta
+
+New-SmbShare -Name Publico-RC -Path $ruta -FullAccess SMR-GL-Total-DirPublico -ReadAccess SMR-GL-Read-DirPublic `
+-ChangeAccess SMR-GL-Change-DirPublico -ConcurrentUserLimit 28 `
+-Description "Carpeta publico para el acceso de usuarios"
