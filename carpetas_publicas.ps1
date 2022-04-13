704480671192de504204e8014f440072450347a8
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-13 17:41:33 +0200

    Update carpetas_publicas.ps1

--- carpetas_publicas.ps1
+++ carpetas_publicas.ps1
@@ -6,8 +6,13 @@ mkdir Comunicacion_Prensa
 mkdir Alergologia
 mkdir Enfermeria
 #
+#
 $ruta = 'X:\Departamentos'
-#Creamos la carpeta que hemos añadido en la variable $ruta
+$Infor = 'X:\Departamentos\Informatica'
+$Aler= 'X:\Departamentos\Alergologia'
+$Enfer = 'X:\Departamentos\Enfermeria'
+$Prensa = 'X:\Departamentos\Comunicacion_Prensa'
+#Creamos la carpeta que hemos añadido en las variables
 #
 New-SmbShare -Name comun_x -Path $ruta -FullAccess Administradores -ReadAccess comun_x -ConcurrentUserLimit 28 `
 -Description "Carpeta común para el acceso de usuarios"
@@ -15,3 +20,15 @@ New-SmbShare -Name comun_x -Path $ruta -FullAccess Administradores -ReadAccess c
 #entonces queda
 #común_x con permisos de acceso en todos los departamentos con todos los grupos globales dentro
 #después estan los grupos locales de cada departamento con permisos de lectura y escritura con los grupos locales respectivos
+#Informatica
+New-SmbShare -Name Informatica -Path $ruta -FullAccess Administradores -ReadAccess Informatica_GL `
+-ChangeAccess Informatica_GL -ConcurrentUserLimit 28 `
+#Alergologia
+New-SmbShare -Name Alergologia -Path $ruta -FullAccess Administradores -ReadAccess Alergologia_GL `
+-ChangeAccess Alergologia_GL -ConcurrentUserLimit 28 `
+#Comunicación y Prensa 
+New-SmbShare -Name Comunicacion_Prensa -Path $ruta -FullAccess Administradores -ReadAccess Comunicacion_Prensa_GL `
+-ChangeAccess Comunicacion_Prensa_GL -ConcurrentUserLimit 28 `
+#Enfermeria
+New-SmbShare -Name Enfermeria -Path $ruta -FullAccess Administradores -ReadAccess Enfermeria_GL `
+-ChangeAccess Enfermeria_GL -ConcurrentUserLimit 28 `
