b54adbef58a5a3acee890234b6b8a3f78ebde88a
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2022-04-26 15:17:23 +0200

    Update permisos_red.ps1

--- permisos_red.ps1
+++ permisos_red.ps1
@@ -23,16 +23,16 @@ Write-Host $Infor
 Write-Host $Aler
 Write-Host $Enfer
 Write-Host $Prensa
-New-SmbShare -Name "comun_x" -Path $ruta -FullAccess "Administradores" -ReadAccess "comun_x" -ConcurrentUserLimit 28
+New-SmbShare -Name "comun_x" -Path $ruta -FullAccess "Administradores" -ReadAccess "comun_x" -ConcurrentUserLimit 28`
 #Lo que se me ha ocurrido para esto es crear un grupo que se llame "común_x" meter los grupos globales a este grupo local
 #entonces queda
 #común_x con permisos de acceso en todos los departamentos con todos los grupos globales dentro
 #después estan los grupos locales de cada departamento con permisos de lectura y escritura con los grupos locales respectivos
 #Informatica
-New-SmbShare -Name "Informatica" -Path $Infor -FullAccess "Administradores" -ReadAccess "Informatica_GL" -ChangeAccess "Informatica_GL" -ConcurrentUserLimit 28
+New-SmbShare -Name "Informatica" -Path $Infor -FullAccess "Administradores" -ReadAccess "Informatica_GL" -ChangeAccess "Informatica_GL" -ConcurrentUserLimit 28`
 #Alergologia
-New-SmbShare -Name "Alergologia" -Path $Aler -FullAccess "Administradores" -ReadAccess "Alergologia_GL" -ChangeAccess "Alergologia_GL" -ConcurrentUserLimit 28 
+New-SmbShare -Name "Alergologia" -Path $Aler -FullAccess "Administradores" -ReadAccess "Alergologia_GL" -ChangeAccess "Alergologia_GL" -ConcurrentUserLimit 28`
 #Comunicación y Prensa 
-New-SmbShare -Name "Comunicacion_Prensa" -Path $Prensa -FullAccess "Administradores" -ReadAccess "Comunicacion_Prensa_GL" -ChangeAccess "Comunicacion_Prensa_GL" -ConcurrentUserLimit 28 
+New-SmbShare -Name "Comunicacion_Prensa" -Path $Prensa -FullAccess "Administradores" -ReadAccess "Comunicacion_Prensa_GL" -ChangeAccess "Comunicacion_Prensa_GL" -ConcurrentUserLimit 28` 
 #Enfermeria
-New-SmbShare -Name "Enfermeria" -Path $Enfer -FullAccess "Administradores" -ReadAccess "Enfermeria_GL" -ChangeAccess "Enfermeria_GL" -ConcurrentUserLimit 28
+New-SmbShare -Name "Enfermeria" -Path $Enfer -FullAccess "Administradores" -ReadAccess "Enfermeria_GL" -ChangeAccess "Enfermeria_GL" -ConcurrentUserLimit 28`
